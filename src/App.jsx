790daa607038fe813b6612da09dee01b49f56eb1
VeetiT <n3tive00@students.oamk.fi>
2024-10-30 15:20:11 +0200

    Added content into the header and also added image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import Header from './components/Header'
 import './style/app.css'
+import image from './assets/pokeball.png'
 
 function App() {
-
+  
   return (
     <div>
+      <Header image={image}/>
       <h1></h1>
     </div>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header({image}) {
+  return (
+    <div>
+        <img src={image}/>
+      <h1>Pokemon Finder</h1>
+      <p>Enter name of a Pokemon. <br></br> Couple different Pokemons could be Pikachu, Turtwig, Kyogre </p>
+    </div>
+  )
+}
+
+export default Header
